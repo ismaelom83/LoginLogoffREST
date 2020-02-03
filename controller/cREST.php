698d7aacb3c723e76046936f927650c6e1208455
ISMAEL HERAS <ismael.hersal@educa.jcyl.es>
2020-02-03 10:23:38 +0100

    trminado el mantenimiento de departamentos

--- controller/cREST.php
+++ controller/cREST.php
@@ -33,6 +33,8 @@ if (isset($_GET["solicitarRest"])) {
         $direccion = $_GET["direccion"];
         //sustituimos los espacios por el simbolo de %20 para que el navegador lo reconozca.
         $nuevadireccion= str_replace(" ", '%20', $direccion);
+        //modificamos para que la ñ la sustituya por la n para que lo reconazca LA API
+        $nuevadireccion= str_replace("ñ", 'n', $direccion);
         //guardamos en unavariable la url para recibir el json expecifico para nuestra direccion.
         $url = "https://maps.googleapis.com/maps/api/geocode/json?address=" . $nuevadireccion . "&key=AIzaSyCrSgHJZQygN2PiJN35GiTuc83XnVHSSlg";
         //trasmite el fichero json a una cadena.
